373f3fe50f66e1f651104ab43b96f8d78062e58d
matthewgeng <matthewgeng2@gmail.com>
2024-01-20 19:02:26 -0500

    add boot comments and util function

---
+++ util.h
@@ -0,0 +1,19 @@
+#ifndef _util_h_
+#define _util_h_ 1
+
+#include <stddef.h>
+
+// conversions
+int a2d(char ch);
+char a2i(char ch, char **src, int base, int *nump);
+void ui2a(unsigned int num, unsigned int base, char *bf);
+void i2a(int num, char *bf);
+
+// memory
+void *memset(void *s, int c, size_t n);
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+
+// defined in boot_util.S
+extern int get_el ( void );
+
+#endif /* util.h */
